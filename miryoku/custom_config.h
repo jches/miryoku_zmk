3fbdc0d7c59e45b31de99d05ea1f09aa03cf369c
John Chesley <j@jches.dev>
2025-03-15 08:50:53 -0400

    use shiftable symbols

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,16 +4,17 @@
 #if defined (MIRYOKU_KEYBOARD_SPLITKB_AURORA_CORNE)
 
 // Add outer columns:
-// Left: LeftBrace, Tilde, (outer left thumb)
-// Right: Pipe. Colon, Underscore
+// Left: LeftBrace/bracket, Tilde/grave, (outer left thumb)
+// Right: Pipe/backslash, (Semi)Colon, Underscore
+// Swap left inner thumb keys
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp LBRC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp PIPE \
-&kp TILDE K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SEMI \
+&kp LBKT  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSLH \
+&kp GRAVE K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SEMI \
 K32       K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp MINUS \
                     K32  K34  K33       K35  K36  K37
 
