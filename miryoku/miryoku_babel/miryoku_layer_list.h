547dc081db59921f584ef1057f81abd84ecdb387
John Chesley <j@jches.dev>
2025-04-07 22:38:53 -0400

    reverse ec11 direction, maybe fix scroll behavior

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -7,13 +7,13 @@
 #if !defined (MIRYOKU_LAYER_LIST)
 
 #define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base",  sensor-bindings = <&inc_dec_kp PG_UP PG_DN &rot_sc_v>; ) \
-MIRYOKU_X(EXTRA,  "Extra", sensor-bindings = <&inc_dec_kp PG_UP PG_DN &rot_sc_v>; ) \
+MIRYOKU_X(BASE,   "Base",  sensor-bindings = <&inc_dec_kp PG_DN PG_UP &rot_sc_v>; ) \
+MIRYOKU_X(EXTRA,  "Extra", sensor-bindings = <&inc_dec_kp PG_DN PG_UP &rot_sc_v>; ) \
 MIRYOKU_X(TAP,    "Tap",    ) \
 MIRYOKU_X(BUTTON, "Button", ) \
-MIRYOKU_X(NAV,    "Nav",   sensor-bindings = <&inc_dec_kp PG_UP PG_DN &rot_sc_v>;   ) \
-MIRYOKU_X(MOUSE,  "Mouse",  ) \
-MIRYOKU_X(MEDIA,  "Media", sensor-bindings = <&inc_dec_kp C_VOL_UP C_VOL_DN &inc_dec_kp C_VOL_UP C_VOL_DN> ;) \
+MIRYOKU_X(NAV,    "Nav",   sensor-bindings = <&inc_dec_kp PG_DN PG_UP &rot_sc_v>;   ) \
+MIRYOKU_X(MOUSE,  "Mouse", sensor-bindings = <&rot_sc_v &rot_sc_v>; ) \
+MIRYOKU_X(MEDIA,  "Media", sensor-bindings = <&inc_dec_kp C_VOL_DN C_VOL_UP &inc_dec_kp C_VOL_DN C_VOL_UP> ;) \
 MIRYOKU_X(NUM,    "Num",    ) \
 MIRYOKU_X(SYM,    "Sym",    ) \
 MIRYOKU_X(FUN,    "Fun",    )
